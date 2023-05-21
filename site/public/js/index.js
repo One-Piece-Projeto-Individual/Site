811e36b0f91d4505e50a20801784d1ef514165a8
torres <gustavo.souza@sptech.school>
2023-05-21 13:32:47 -0300

    update8.0

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -21,8 +21,53 @@ var contexto = {
   };
   var contexto = contexto.texto;
 
+var frutas = {
+    texto : ` As Akuma no Mi são frutas místicas encontradas no mundo de One Piece que concedem<br>
+    habilidades especiais a quem as consome. Existem três tipos principais de<br>
+     Akuma no Mi: Paramecia, Zoan e Logia, porém quando consumida você fica incapacitado<br>
+     de nadar por conta da maldição dos mares.<br><br>
 
-  
+   As Akuma no Mi do tipo Paramecia concedem poderes extraordinários aos usuários, mas<br>
+    não alteram sua fisiologia. Os usuários de Paramecia podem ter habilidades variadas e<br>
+     únicas, como a capacidade de manipular partes do corpo, criar substâncias especiais,<br>
+      controlar elementos não naturais, entre outros exemplos. Alguns personagens famosos que<br>
+       possuem Akuma no Mi do tipo Paramecia incluem Monkey D. Luffy, que possui a Gomu Gomu no<br>
+        Mi, permitindo-lhe esticar seu corpo como borracha, e Eustass Kid, que possui a Magnetic Magnetic<br>
+         Fruit e pode manipular campos magnéticos.<br><br>
+   
+   As Akuma no Mi do tipo Zoan permitem que os usuários se transformem em uma forma<br>
+    animal, híbrida (meio humano, meio animal) ou completamente humana. Essas transformações<br>
+     concedem força física aprimorada, resistência e habilidades especiais relacionadas ao animal<br>
+      em questão. Exemplos de personagens com Akuma no Mi do tipo Zoan são Tony Tony Chopper, que<br>
+       possui a Human Human Fruit, permitindo-lhe se transformar em diferentes formas de renas, e X<br>
+        Drake, que possui a Allosaurus Dinosaur Fruit, permitindo-lhe se transformar em um allossauro.<br><br>
+   
+   As Akuma no Mi do tipo Logia permitem que os usuários se tornem, controlem e manipulem substâncias<br>
+    elementares. Isso inclui elementos como fogo, gelo, eletricidade, fumaça e muito mais. Os usuários<br>
+     de Logia têm a capacidade de se tornarem intangíveis, transformando seus corpos em sua substância<br>
+      elemental e evitando danos físicos. Alguns exemplos notáveis de Akuma no Mi do tipo Logia são<br>
+       a Mera Mera no Mi, que concede o poder do fogo a seu usuário, e a Goro Goro no Mi, que concede<br>
+        o poder da eletricidade.<br><br>
+
+   Zoan Míticas permitem ao usuário se transformar em uma criatura mítica ou lendária<br>.
+
+   Existem várias Zoan Míticas apresentadas na série, cada uma com suas características<br>
+    únicas. Alguns exemplos notáveis incluem:<br>
+
+   Hito Hito no Mi, Modelo: Daibutsu (Fruta Humano-Humano, Modelo: Grande Buda): Permite<br>
+    a transformação em uma forma gigantesca semelhante a um Buda, concedendo imensa força e<br>
+     resistência. É possuída por Sengoku, um almirante da Marinha.<br><br>
+
+   Tori Tori no Mi, Modelo: Fênix (Fruta Pássaro, Modelo: Fênix): Permite ao usuário se<br>
+    transformar em uma fênix, uma criatura lendária que tem a capacidade de renascer das<br>
+     cinzas. Foi consumida por Marco, o comandante dos Piratas do Barba Branca.<br><br>
+
+   Inu Inu no Mi, Modelo: Kyubi no Kitsune (Fruta Cão-Cão, Modelo: Raposa de Nove<br>
+    Caudas): Permite a transformação em uma raposa com nove caudas, uma criatura mitológica<br>
+     com poderes sobrenaturais. É possuída por Kozuki Momonosuke.<br><br><br>
+     `,
+}
+  var frutas = frutas.texto;
 
   
 
